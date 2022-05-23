9821abd9903941092ff232609ee78afdc69d2b59
Lucien <lucien@luciens-mbp-1.home>
2022-05-23 11:44:11 +0200

    Added C2W3

---
+++ Bayesian-Statistics-Techniques-Models/Week3/assignment_7B_mod1.R
@@ -0,0 +1,49 @@
+library("car")  # load the 'car' package
+data("Anscombe")  # load the data set
+head(Anscombe)  # look at the first few lines of the data
+pairs(Anscombe)  # scatter plots for each pair of variables
+
+library("rjags")
+
+mod_string = " model {
+    for (i in 1:length(education)) {
+        education[i] ~ dnorm(mu[i], prec)
+        mu[i] = b0 + b[1]*income[i] + b[2]*young[i]
+    }
+    
+    b0 ~ dnorm(0.0, 1.0/1.0e6)
+    for (i in 1:2) {
+        b[i] ~ dnorm(0.0, 1.0/1.0e6)
+    }
+    
+    prec ~ dgamma(1.0/2.0, 1.0*1500.0/2.0)
+    	## Initial guess of variance based on overall
+    	## variance of education variable. Uses low prior
+    	## effective sample size. Technically, this is not
+    	## a true 'prior', but it is not very informative.
+    sig2 = 1.0 / prec
+    sig = sqrt(sig2)
+} "
+
+data_jags = as.list(Anscombe)
+
+set.seed(72)
+
+params = c("b", "sig")
+
+inits = function() {
+    inits = list("b"=rnorm(2,0.0,100.0), "prec"=rgamma(1,1.0,1.0))
+}
+
+mod = jags.model(textConnection(mod_string), data=data_jags, inits=inits, n.chains=3)
+update(mod, 1000) # burn-in
+
+mod_sim = coda.samples(model=mod,
+                        variable.names=params,
+                        n.iter=5000)
+
+mod_csim = do.call(rbind, mod_sim) # combine multiple chains
+
+summary(mod_sim)
+
+dic.samples(mod,n.iter=1e5)
---
+++ Bayesian-Statistics-Techniques-Models/Week3/assignment_8.R
@@ -0,0 +1,40 @@
+data("PlantGrowth")
+head(PlantGrowth)
+
+library("rjags")
+
+mod_string = " model {
+    for (i in 1:length(y)) {
+        y[i] ~ dnorm(mu[grp[i]], prec[grp[i]])
+    }
+
+    for (j in 1:3) {
+        mu[j] ~ dnorm(0.0, 1.0/1.0e6)
+    }
+
+    for (j in 1:3) {
+        prec[j] ~ dgamma(5/2.0, 5*1.0/2.0)
+        sig[j] = sqrt( 1.0 / prec[j] )
+    }
+} "
+
+set.seed(82)
+str(PlantGrowth)
+data_jags = list(y=PlantGrowth$weight,
+              grp=as.numeric(PlantGrowth$group))
+
+params = c("mu", "sig")
+
+inits = function() {
+    inits = list("mu"=rnorm(3,0.0,100.0), "prec"=rgamma(3,1.0,1.0))
+}
+
+mod = jags.model(textConnection(mod_string), data=data_jags, inits=inits, n.chains=3)
+update(mod, 1e3)
+
+mod_sim = coda.samples(model=mod,
+                        variable.names=params,
+                        n.iter=5e3)
+mod_csim = as.mcmc(do.call(rbind, mod_sim)) # combined chains
+
+summary(mod_sim)
